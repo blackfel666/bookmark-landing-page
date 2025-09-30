0c6611a4c418b935be687c87cc86b9acb8895582
blackfel666 <fv10644@gmail.com>
2025-09-30 13:59:06 +0200

    add styles & change errors

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,9 @@ const nav = document.querySelector("#nav");
 const open1 = document.querySelector("#open1");
 const close1 = document.querySelector("#close1");
 
+const emailInput = document.getElementById('email');
+const emailError = document.getElementById('errorEmail');
+
 open1.addEventListener("click", () => {
     nav.classList.add("visible");
 })
@@ -11,3 +14,15 @@ open1.addEventListener("click", () => {
 close1.addEventListener("click", () => {
     nav.classList.remove("visible");
 })
+
+emailInput.addEventListener('input', function () {
+  if (emailInput.validity.typeMismatch) {
+    emailError.textContent = 'whoops, make sure it´s an email';
+    emailError.style.display = 'block';
+    emailInput.classList.add('invalid');
+  } else {
+    emailError.textContent = '';
+    emailError.style.display = 'none';
+    emailInput.classList.remove('invalid');
+  }
+});
